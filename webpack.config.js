8cfa4811331695dd2005f0f7287ef34ef7669922
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-01 18:44:48 -0300

    config SASS

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,29 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
   },
   devServer: {
     contentBase: '.dist',
@@ -22,6 +41,7 @@ module.exports = {
   },
   externals: {
     react: 'react',
+    axios: 'axios',
     'react-dom': 'reactDOM'
   },
   plugins: [
