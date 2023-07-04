bb29192ad9bbb3b741952484898b75c93564c99e
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-04 17:43:14 -0300

    prevent Login to submit form if its invalid

--- tests/presentation/pages/login/login.spec.tsx
+++ tests/presentation/pages/login/login.spec.tsx
@@ -120,4 +120,12 @@ describe('Login Component', () => {
     simulateValidSubmit(sut)
     expect(authenticationSpy.callsCaount).toBe(1)
   })
+
+  it('Should not call Authentication if form is invalid', () => {
+    const validationError = faker.lorem.words()
+    const { sut, authenticationSpy } = makeSut({ validationError })
+    populateEmailField(sut)
+    fireEvent.submit(sut.getByTestId('form'))
+    expect(authenticationSpy.callsCaount).toBe(0)
+  })
 })
