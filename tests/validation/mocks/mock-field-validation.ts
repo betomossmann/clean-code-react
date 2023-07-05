d47f08d8cf5d32ef1b178bf1f1135f2baa64aaed
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-05 17:33:33 -0300

    ensure ValidationComposite returns falsy if there is no error

---
+++ tests/validation/mocks/mock-field-validation.ts
@@ -0,0 +1,11 @@
+import { type FieldValidation } from '@/validation/protocols'
+
+export class FieldValidationSpy implements FieldValidation {
+  error: Error = null
+
+  constructor (readonly field: string) {}
+
+  validate (value: string): Error {
+    return this.error
+  }
+}
---
+++ tests/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,40 @@
+import { ValidationComposite } from '@/validation/validation-composite/validation-composite'
+import { FieldValidationSpy } from '@/tests/validation/mocks'
+
+import { faker } from '@faker-js/faker'
+
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (fieldName: string): SutTypes => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
+  ]
+  const sut = new ValidationComposite(fieldValidationsSpy)
+  return {
+    sut,
+    fieldValidationsSpy
+  }
+}
+
+describe('ValidationComposite', () => {
+  it('Should return erro if any validation fails', () => {
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationsSpy } = makeSut(fieldName)
+    const errorMessage = faker.lorem.words()
+    fieldValidationsSpy[0].error = new Error(errorMessage)
+    fieldValidationsSpy[1].error = new Error(faker.lorem.words())
+    const error = sut.validate(fieldName, faker.lorem.words())
+    expect(error).toBe(error)
+  })
+
+  it('Should validaiton return falsy there is no error', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+    const error = sut.validate(fieldName, faker.lorem.word())
+    expect(error).toBeFalsy()
+  })
+})
