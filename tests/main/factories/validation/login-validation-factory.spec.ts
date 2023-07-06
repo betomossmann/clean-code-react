12f3940019ccc80fe447f23a5a35e060d4d0cb94
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-06 10:38:10 -0300

    ensure LoginValidationComposite uses correct validations

---
+++ tests/main/factories/validation/login-validation-factory.spec.ts
@@ -0,0 +1,17 @@
+import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
+import { ValidationComposite } from '@/validation/validation-composite'
+import { RequiredFieldValidation } from '@/validation/required-field'
+import { MinLengthValidation } from '@/validation/min-length'
+import { EmailValidation } from '@/validation/email'
+
+describe('LoginValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeLoginValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      new RequiredFieldValidation('email'),
+      new EmailValidation('email'),
+      new RequiredFieldValidation('password'),
+      new MinLengthValidation('password', 5)
+    ]))
+  })
+})
