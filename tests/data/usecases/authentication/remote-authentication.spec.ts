2daa981727e74ca79eb6d3597d0b36b932562dfb
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-17 16:05:05 -0300

    ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 500

--- tests/data/usecases/authentication/remote-authentication.spec.ts
+++ tests/data/usecases/authentication/remote-authentication.spec.ts
@@ -55,7 +55,7 @@ describe('RemoteAuthentication', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
-  it('Should throw UnexpectedError if HttpPostClient returns 404', async () => {
+  it('Should throw UnexpectedError if HttpPostClient returns 500', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
       statusCode: HttpStatusCode.serverError
@@ -64,7 +64,7 @@ describe('RemoteAuthentication', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
-  it('Should throw UnexpectedError if HttpPostClient returns 500', async () => {
+  it('Should throw UnexpectedError if HttpPostClient returns 404', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
       statusCode: HttpStatusCode.notFound
