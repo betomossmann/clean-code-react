7bcd22bbc7edab96a95e548df06ebd974b1ce8e2
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-17 14:55:10 -0300

    add AddAccount usecase

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,12 @@
+import { type AccountModel } from '@/domain/models'
+
+export type AddAccountParams = {
+  name: string
+  email: string
+  password: string
+  passwordConfirmation: string
+}
+
+export interface AddAccount {
+  add: (params: AddAccountParams) => Promise<AccountModel>
+}
