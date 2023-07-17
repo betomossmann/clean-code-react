388792367cf34fe3f1aed79c3e98397076d253a9
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-17 15:02:31 -0300

    rename data mocks refactor

--- tests/data/mocks/mock-http-post.ts
+++
@@ -1,8 +0,0 @@
-import { type HttpPostParams } from '@/data/protocols/http'
-
-import { faker } from '@faker-js/faker'
-
-export const mockPostRquest = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.helpers.objectValue({ new: 'any_value' })
-})
--- tests/data/mocks/mock-http.ts
+++ tests/data/mocks/mock-http.ts
@@ -1,5 +1,12 @@
 import { type HttpPostParams, type HttpPostClient, type HttpResponse, HttpStatusCode } from '@/data/protocols/http'
 
+import { faker } from '@faker-js/faker'
+
+export const mockPostRquest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.helpers.objectValue({ new: 'any_value' })
+})
+
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
   body?: T
