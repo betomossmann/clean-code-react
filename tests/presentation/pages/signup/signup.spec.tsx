18e04283ed55e630ce855d096541ce2557cadd3c
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-18 13:04:29 -0300

    ensure SignUp show valid email status if validation succeeds

--- tests/presentation/pages/signup/signup.spec.tsx
+++ tests/presentation/pages/signup/signup.spec.tsx
@@ -71,4 +71,10 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'name')
     Helper.testStatusForField(sut, 'name')
   })
+
+  it('Should show valid email state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'email')
+    Helper.testStatusForField(sut, 'email')
+  })
 })
