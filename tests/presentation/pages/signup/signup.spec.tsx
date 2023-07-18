580819f2ef49483064aa98a5538a68bc86171abd
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-18 13:04:08 -0300

    ensure SignUp show valid name status if validation succeeds

--- tests/presentation/pages/signup/signup.spec.tsx
+++ tests/presentation/pages/signup/signup.spec.tsx
@@ -65,4 +65,10 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'passwordConfirmation')
     Helper.testStatusForField(sut, 'passwordConfirmation', validationError)
   })
+
+  it('Should show valid name state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'name')
+    Helper.testStatusForField(sut, 'name')
+  })
 })
