fe3decdf77be6fa9c8e05965e5d0cd907c66edaa
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-18 09:49:23 -0300

    ensure SignUp starts with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -2,23 +2,31 @@ import Styles from './signup-styles.scss'
 import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components'
 import { Context } from '@/presentation/contexts/form'
 
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
 
 const SignUp: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    nameError: 'Campo obrigatório',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    passwordConfirmationError: 'Campo obrigatório',
+    mainError: ''
+  })
+
   return (
     <>
       <div className={Styles.signup}>
         <LoginHeader />
-        <Context.Provider value={{ state: {} }}>
+        <Context.Provider value={{ state }}>
           <form className={Styles.form}>
             <h2>Criar Conta</h2>
             <Input type='text' name='name' placeholder='Digite seu nome' />
             <Input type='email' name='email' placeholder='Digite seu e-mail' />
             <Input type='password' name='password' placeholder='Digite sua senha' />
             <Input type='password' name='passwordConfirmation' placeholder='Repita sua senha' />
-            <button className={Styles.submit} type="submit">Criar</button>
-            <Link to='/signup' className={Styles.link}>Voltar para Login</Link>
+            <button data-testid="submit" disabled className={Styles.submit} type="submit">Criar</button>
+            <span className={Styles.link}>Voltar para Login</span>
             <FormStatus />
           </form>
         </Context.Provider>
---
+++ tests/presentation/pages/signup/signup.spec.tsx
@@ -0,0 +1,46 @@
+import { SignUp } from '@/presentation/pages'
+
+import React from 'react'
+import { type RenderResult, render } from '@testing-library/react'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(
+    <SignUp />
+  )
+  return {
+    sut
+  }
+}
+
+const testChildCount = (sut: RenderResult, fieldName: string, count: number): void => {
+  const el = sut.getByTestId(fieldName)
+  expect(el.childElementCount).toBe(count)
+}
+
+const testButtonDisabled = (sut: RenderResult, fieldName: string, isDisabled: boolean): void => {
+  const button = sut.getByTestId(fieldName) as HTMLButtonElement
+  expect(button.disabled).toBe(isDisabled)
+}
+
+const testStatusForField = (sut: RenderResult, fieldName: string, validationError?: string): void => {
+  const fieldStatus = sut.getByTestId(`${fieldName}-status`)
+  expect(fieldStatus.title).toBe(validationError || 'Tudo certo!')
+  expect(fieldStatus.textContent).toBe(validationError ? '🔴' : '🟢')
+}
+
+describe('SignUp Component', () => {
+  it('Should start with initial state', () => {
+    const validationError = 'Campo obrigatório'
+    const { sut } = makeSut()
+    testChildCount(sut, 'error-wrap', 0)
+    testButtonDisabled(sut, 'submit', true)
+    testStatusForField(sut, 'name', validationError)
+    testStatusForField(sut, 'email', validationError)
+    testStatusForField(sut, 'password', validationError)
+    testStatusForField(sut, 'passwordConfirmation', validationError)
+  })
+})
