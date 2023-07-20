d026ae68036551cf6f4223e8a2e16197f4daad2f
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-20 19:53:56 -0300

    change text for error intermittent on faker

--- tests/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ tests/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -7,19 +7,19 @@ const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation
 
 describe('CompareFieldsValidation', () => {
   it('Should return error if compare is invalid', () => {
-    const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const field = 'any_field'
+    const fieldToCompare = 'other_field'
     const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({
-      [field]: faker.lorem.words(5),
-      [fieldToCompare]: faker.lorem.words(6)
+      [field]: 'any_value',
+      [fieldToCompare]: 'other_value'
     })
     expect(error).toEqual(new InvalidFieldError())
   })
 
   it('Should return falsy if compare is valid', () => {
-    const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const field = 'any_field'
+    const fieldToCompare = 'other_field'
     const value = faker.lorem.word()
     const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({
