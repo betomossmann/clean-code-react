669567d9e2d83560cec188dc13d628af9ab623a7
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-20 07:34:28 -0300

    ensure Login begin with correct initial state cypress

--- tests/e2e/cypress/integration/login.spec.ts
+++ tests/e2e/cypress/integration/login.spec.ts
@@ -3,6 +3,9 @@ describe('Login', () => {
     cy.visit('login')
   })
   it('Should load with correct initial state', () => {
-
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo Obrigatório')
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Campo Obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
