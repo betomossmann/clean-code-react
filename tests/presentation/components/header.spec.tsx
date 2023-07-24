aa41cfdb870075a503fe2bca4b16d4946a310f32
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-24 13:21:37 -0300

    ensure logout calls setCurrentAccount with null

--- tests/presentation/components/header.spec.tsx
+++ tests/presentation/components/header.spec.tsx
@@ -1,5 +1,36 @@
-describe('', () => {
-  it('Should', () => {
+import { mockAccountModel } from '@/tests/domain/mocks'
+import { type AccountModel } from '@/domain/models'
+import { ApiContext } from '@/presentation/contexts'
+import { Header } from '@/presentation/components'
 
+import React from 'react'
+import { Router } from 'react-router-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { createMemoryHistory, type MemoryHistory } from 'history'
+
+type SutTypes = {
+  history: MemoryHistory
+  setCurrentAccountMock: (account: AccountModel) => void
+}
+
+const makeSut = (account = mockAccountModel()): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  const setCurrentAccountMock = jest.fn()
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => account }}>
+      <Router location={history.location} navigator={history}>
+        <Header />
+      </Router>
+    </ApiContext.Provider>
+  )
+  return { history, setCurrentAccountMock }
+}
+
+describe('Header Component', () => {
+  it('Should call setCurrentAccount with null', () => {
+    const { history, setCurrentAccountMock } = makeSut()
+    fireEvent.click(screen.getByTestId('logout'))
+    expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
+    expect(history.location.pathname).toBe('/login')
   })
 })
