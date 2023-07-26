3ed2e8ec21970fe219aefa0d449eede8b62671c8
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-26 08:12:15 -0300

    ensure SurveyList logout on AccessDeniedError

--- tests/presentation/pages/survey-list/survey-list.spec.tsx
+++ tests/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,6 +1,6 @@
 import { SurveyList } from '@/presentation/pages'
 import { ApiContext } from '@/presentation/contexts'
-import { UnexpectedError } from '@/domain/error'
+import { AccessDeniedError, UnexpectedError } from '@/domain/error'
 import { type AccountModel } from '@/domain/models'
 import { LoadSurveyListSpy, mockAccountModel } from '@/tests/domain/mocks'
 
@@ -65,14 +65,21 @@ describe('SurveyList Components', () => {
     expect(screen.queryByTestId('error')).toHaveTextContent(error.message)
   })
 
+  test('Should logout on AccessDeniedError', async () => {
+    const loadSurveyListSpy = new LoadSurveyListSpy()
+    jest.spyOn(loadSurveyListSpy, 'loadAll').mockRejectedValueOnce(new AccessDeniedError())
+    const { setCurrentAccountMock, history } = makeSut(loadSurveyListSpy)
+    await waitFor(() => screen.getByRole('heading'))
+    expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
+    expect(history.location.pathname).toBe('/login')
+  })
+
   it('Should call LoadSurveyList on reload', async () => {
     const loadSurveyListSpy = new LoadSurveyListSpy()
     jest.spyOn(loadSurveyListSpy, 'loadAll').mockRejectedValueOnce(new UnexpectedError())
     makeSut(loadSurveyListSpy)
     await waitFor(() => screen.getByRole('heading'))
-
     fireEvent.click(await screen.findByTestId('reload'))
-
     expect(loadSurveyListSpy.callsCount).toBe(1)
     await waitFor(() => screen.getByRole('heading'))
   })
