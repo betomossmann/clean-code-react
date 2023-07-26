5181320c4818f9738e3b85e545ca3e3e972ef8ec
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-26 09:20:58 -0300

    change test to ensure values are diferentes fix

--- tests/validation/validators/min-length/min-length-validation.spec.ts
+++ tests/validation/validators/min-length/min-length-validation.spec.ts
@@ -21,8 +21,8 @@ describe('MinLengthValidation', () => {
   })
 
   it('Should return falsy if field does not exists in schema', () => {
-    const sut = makeSut(faker.database.column())
-    const error = sut.validate({ [faker.database.column()]: faker.string.alpha(5) })
+    const sut = makeSut('any_field')
+    const error = sut.validate({ invalidField: faker.string.alpha(5) })
     expect(error).toBeFalsy()
   })
 })
