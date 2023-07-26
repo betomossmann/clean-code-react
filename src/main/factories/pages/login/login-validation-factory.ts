db26f9e62b8b7bdffe739454a0913b48e497d089
Beto Mossmann <gilbertomossmann@gmail.com>
2023-07-26 08:21:40 -0300

    change validation factories refactor

--- src/main/factories/pages/login/login-validation-factory.ts
+++ src/main/factories/pages/login/login-validation-factory.ts
@@ -1,9 +1,7 @@
 import { ValidationComposite } from '@/validation/validation-composite'
 import { ValidationBuilder as Builder } from '@/validation/builder'
 
-export const makeLoginValidation = (): ValidationComposite => {
-  return ValidationComposite.build([
-    ...Builder.field('email').required().email().build(),
-    ...Builder.field('password').required().min(5).build()
-  ])
-}
+export const makeLoginValidation = (): ValidationComposite => ValidationComposite.build([
+  ...Builder.field('email').required().email().build(),
+  ...Builder.field('password').required().min(5).build()
+])
--- src/main/factories/pages/signup/signup-validation-factory.ts
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -1,11 +1,9 @@
 import { ValidationComposite } from '@/validation/validation-composite'
 import { ValidationBuilder as Builder } from '@/validation/builder'
 
-export const makeSignUpValidation = (): ValidationComposite => {
-  return ValidationComposite.build([
-    ...Builder.field('name').required().min(5).build(),
-    ...Builder.field('email').required().email().build(),
-    ...Builder.field('password').required().min(5).build(),
-    ...Builder.field('passwordConfirmation').required().sameAs('password').build()
-  ])
-}
+export const makeSignUpValidation = (): ValidationComposite => ValidationComposite.build([
+  ...Builder.field('name').required().min(5).build(),
+  ...Builder.field('email').required().email().build(),
+  ...Builder.field('password').required().min(5).build(),
+  ...Builder.field('passwordConfirmation').required().sameAs('password').build()
+])
